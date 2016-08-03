300685dff9ef0892778fd97ac7df627ee6104af8
Jim Bulkowski <jim.b@paperelectron.com>
2016-08-03 14:33:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-request
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+
+/**
+ *
+ * @module index
+ */
+
+module.exports = [
+  require('pomegranate-request'),
+  require('./lib/RequestEndpoints')
+]
